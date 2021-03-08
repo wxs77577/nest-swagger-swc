c545cfd059e3ff26d06c50a067aa57f90ba4bc23
wxs@sj <wu-xuesong@qq.com>
2021-03-08 15:46:37 +0800

    add swc

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -6,7 +6,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
+  getHello(@Body() body): string {
     return this.appService.getHello();
   }
 }
